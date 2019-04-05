916f6bdab5f6edae123d0301f04070b9cd73fb13
Shreeviknesh <luckyboy1998@gmail.com>
2019-04-06 00:12:41 +0530

    Added WTForms

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_pymongo import PyMongo
 
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'dbook'
 app.config['MONGO_URI'] = 'mongodb://shree:strongpassword123@ds060369.mlab.com:60369/dbook'
+app.config['SECRET_KEY'] = '97542ec161edf850af891551159ea600'
 mongo = PyMongo(app)
 
 @app.route('/')
@@ -15,11 +17,13 @@ def index_route():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login_route():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register_route():
-    return render_template('register.html')
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=80, debug=True)
