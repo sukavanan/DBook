55ca8256b22e0490d6fa2c3074a0db8ca47748a9
Shreeviknesh <luckyboy1998@gmail.com>
2019-04-05 22:01:16 +0530

    Second commit - Frontend basic done

--- server.py
+++ server.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'dbook'
+app.config['MONGO_URI'] = 'mongodb://shree:strongpassword123@ds060369.mlab.com:60369/dbook'
+mongo = PyMongo(app)
 
 @app.route('/')
 def index_route():
-    return '<h1>Welcome.</h1>'
+    if 'username' in session:
+        return f'You are logged in as {session["username"]}!'
+    
+    return render_template('index.html', align_center=True)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login_route():
+    return render_template('login.html')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register_route():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=80, debug=True)
